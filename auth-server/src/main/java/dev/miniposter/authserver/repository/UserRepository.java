71d2c8f8106cfefcb1856985b5d37656cb686f58
JackRaidenPH <jraiden.ph@gmail.com>
2024-09-02 05:22:17 +0300

    fix: fix wrong id type for UserRepository

--- auth-server/src/main/java/dev/miniposter/authserver/repository/UserRepository.java
+++ auth-server/src/main/java/dev/miniposter/authserver/repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
     boolean existsByUsername(String username);
